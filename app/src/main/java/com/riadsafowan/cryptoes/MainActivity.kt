d6041f084ec3d7448d852451fd0b72ccd77ab436
Riad Safowan <riad.safowan@northsouth.edu>
2022-07-05 12:03:55 +0600

    UI list screen working

--- app/src/main/java/com/riadsafowan/cryptoes/MainActivity.kt
+++ app/src/main/java/com/riadsafowan/cryptoes/MainActivity.kt
@@ -10,21 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.riadsafowan.cryptoes.presentation.Screen
+import com.riadsafowan.cryptoes.presentation.coin_list.CoinListScreen
 import com.riadsafowan.cryptoes.presentation.ui.theme.CryptoesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CryptoesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            CoinListScreen(navController = rememberNavController())
         }
     }
 }
---
+++ app/src/main/java/com/riadsafowan/cryptoes/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,52 @@
+package com.riadsafowan.cryptoes.presentation.coin_list
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.riadsafowan.cryptoes.presentation.coin_list.components.CoinListItem
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel()
+){
+    val state = viewModel.state.value
+    val context = LocalContext.current
+    Box(modifier = Modifier.fillMaxSize()){
+        LazyColumn(modifier = Modifier.fillMaxSize()){
+            items(state.coins.size){coin->
+                CoinListItem(coin = state.coins[coin]){
+                    Toast.makeText(context, it.name, Toast.LENGTH_SHORT).show()
+//                    navController.navigate(Screen.CoinDetailScreen.route+"/${it.id}")
+                }
+            }
+        }
+        if (state.error.isNotBlank()){
+            Text(text = state.error,
+            color= MaterialTheme.colors.error,
+            textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+                )
+        }
+        if (state.isLoading){
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
